7484ded6393faa07f41fa6da4e3ee2ea5c0c58e7
ShirdelY <83230748+ShirdelY@users.noreply.github.com>
2022-06-02 18:46:19 -0400

    move code to src

--- src/EasyMode.java
+++ src/EasyMode.java
@@ -1,16 +1,16 @@
+package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class EasyMode extends JFrame {
+public class EasyMode extends JPanel {
     private char[][] grid = new char[5][6];
     private char[] key = new char[5];
-    JPanel panel2 = new JPanel();
     EasyMode(char[] key)
     {
         this.key = key;
         //JPanel panel = new JPanel();
-        panel2.setBounds(50,50, 500, 500);
-        panel2.setBackground(Color.YELLOW);
+        setBounds(50,50, 500, 500);
+        setBackground(Color.YELLOW);
 
     }
 }
