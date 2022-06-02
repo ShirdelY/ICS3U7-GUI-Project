c4e296a7b8f8cfcda9d52cfa495c5f29e396dfb1
ShirdelY <83230748+ShirdelY@users.noreply.github.com>
2022-06-02 02:04:39 +0000

    add titlescreen method

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-class Main {
+import javax.swing.*;
+class Main extends JFrame{
+  titleScreen() {
+    setLayout(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+    setSize(600, 1200);
+    JButton mainMenu = new JButton("Main Menu");
+    mainMenu.setBounds(200, 800, 200, 100);
+    add(mainMenu);
+  }
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    new titleScreen();
   }
 }
