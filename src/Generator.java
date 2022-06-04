8a62c9e15406a82af4e8be9d54535ed1a5228ff0
ShirdelY <shirdel.yan@gmail.com>
2022-06-04 10:59:50 -0400

    Update Generator.java

--- src/Generator.java
+++ src/Generator.java
@@ -1,13 +1,25 @@
-public class Generator {
+import java.io.*;
+import java.util.*;
+public class Generator  {
     private int length;
-
-    Generator(int length) throws Exception {
+    File file5 = new File("5LetterKeyWords.txt");
+    Scanner five = new Scanner(file5);
+    File file7 = new File("7LetterKeyWords.txt");
+    Scanner seven = new Scanner(file7);
+    Generator(int length) {
         this.length = length;
-        if (length != 5 || length != 7)
-        {
-            throw new Exception("Invalid length");
-        }
-        
     }
 
+    String getRandom() {
+        if (length == 5)
+        {
+            String result;
+            //source file for 5 letter words has 586 lines
+            //MUST BE UPDATED IF FILE IS CHANGED
+            for (int i = 0; i < Math.round(Math.random() * 586); i++)
+            {
+                result = five.nextLine();
+            }
+        }
+    }
 }
