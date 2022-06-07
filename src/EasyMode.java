a15aac886a852cf226c74396cf7eb08af722d84f
ShirdelY <83230748+ShirdelY@users.noreply.github.com>
2022-06-07 09:16:30 -0400

    Update EasyMode.java

--- src/EasyMode.java
+++ src/EasyMode.java
@@ -5,26 +5,18 @@ import java.awt.event.KeyListener;
 import java.util.*;
 
 public class EasyMode extends JFrame {
+    private char[][] grid = new char[5][6];
+    private char[] keyword = new char[5];
+    JFrame frame = new JFrame();
     EasyMode (String key) {
-        char[][] grid = new char[5][6];
-        char[] keyword = new char[5];
         char input;
         for (int i = 0; i < 5; i++) keyword[i] = key.charAt(i);
-        JFrame frame = new JFrame();
         frame.setLayout(null);
         //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.WHITE);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setSize(600,650);
-        //create scanner for keyboard input
-        Scanner keyboard = new Scanner(System.in);
-        //listen for input
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-            }
-        }
+        frame.addKeyListener();
     }
 }
