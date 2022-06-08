8a678597b4e292f3e8df1b04d4f384a9b2503fa2
Shiza Charania <86497160+shizacharania@users.noreply.github.com>
2022-06-08 10:56:36 -0400

    adding comments

--- src/Start.java
+++ src/Start.java
@@ -1,7 +1,7 @@
 /**
- * 
+ * Start.java - version 1
+ * This class displays the title page of our program with the start button to go to the main menu
  * @author shizacharania
- *
  */
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,34 +13,52 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Start extends JFrame implements ActionListener {
-	JButton mainmenu;
+	// declaring button to start the game
+	JButton start;
+	/**
+	 * Constructor (special method) for the Jframe GUI - specifically showing the title, and 1 button
+	 * @param - none
+	 * @return - none
+	 */
 	Start() {
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 
+		// game title
 		JLabel label1 = new JLabel("HELLO WORDL"); //center this after
 		label1.setBounds(250, 35, 100, 50);
 		add(label1);
 		
-		mainmenu = new JButton("Main Menu");
-		mainmenu.setBounds(200, 475, 200, 75);
-		add(mainmenu);
-		mainmenu.addActionListener(this);
+		// start button
+		start = new JButton("Start");
+		start.setBounds(200, 475, 200, 75);
+		add(start);
+		start.addActionListener(this);
 		
 		pack();
 		setSize(600,650);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	/**
+	 * main method for calling the Start() constructor
+	 * @param args
+	 * @return - none
+	 */
 	public static void main(String[] args) {
 		new Start();
 	}
+	/**
+	 * actionPerformed method to manipulate and set specific instructions for the button
+	 * @param ActionEvent object
+	 * @return - none
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource() == mainmenu) {
-			System.out.println("main menu");
-			// if main menu is pressed, we go to the main menu by calling it
+		if (e.getSource() == start) {
+			System.out.println("start");
+			// if start is pressed, we go to the main menu by calling it
 		}
 	}
 }
