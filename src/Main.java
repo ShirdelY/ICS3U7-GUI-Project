3d714d0d9d74953953d582184f1fe4405c652617
Shiza Charania <86497160+shizacharania@users.noreply.github.com>
2022-06-15 17:46:02 -0400

    Manipulating new text file and adding its methods

--- src/Main.java
+++ src/Main.java
@@ -11,36 +11,50 @@ import java.util.*;
 public class Main extends JFrame {
 	//source files for key words
 	final private static File SOURCE5 = new File("src/5LetterKeyWords.txt");
+	final private static File VALIDSOURCE5 = new File("src/5LetterValidWords.txt");
 	final private static File SOURCE7 = new File("src/7LetterKeyWords.txt");
 	//source file lengths
-	final private static int FIVELENGTH = 12972;
+	final private static int FIVELENGTH = 586;
+	final private static int VALIDFIVELENGTH = 12972;
 	final private static int SEVENLENGTH = 500;
 	//create arrays for words
-	private static String[] WORDS5 = new String[12972];
-	private static String[] WORDS7 = new String[500];
+	private static String[] words5 = new String[586];
+	private static String[] validwords5 = new String[12972];
+	private static String[] words7 = new String[500];
 
 	public static void main(String[] args) throws IOException{
 		//import keywords
-		WORDS5 = importWords5();
-		WORDS7 = importWords7();
+		words5 = importWords5();
+		validwords5 = importValidWords5();
+		words7 = importWords7();
 		//create new title screen
 		new Start();
-		//import keywords
 	}
 
 	public static String[] importWords5() throws IOException {
 		//create temporary non final arrays to fill
 		String[] words5temp = new String[FIVELENGTH];
-		String[] words7temp = new String[SEVENLENGTH];
 		//create scanners to import txt files to arrays
 		Scanner fiveInput = new Scanner(SOURCE5);
-		Scanner sevenInput = new Scanner(SOURCE7);
 		//import txt files
 		for (int i = 0; i < FIVELENGTH; i++)
 		{
 			words5temp[i] = fiveInput.nextLine();
 		}
 		return words5temp;
+	}
+	
+	public static String[] importValidWords5() throws IOException {
+		//create temporary non final arrays to fill
+		String[] validwords5temp = new String[VALIDFIVELENGTH];
+		//create scanners to import txt files to arrays
+		Scanner validFiveInput = new Scanner(VALIDSOURCE5);
+		//import txt files
+		for (int i = 0; i < VALIDFIVELENGTH; i++)
+		{
+			validwords5temp[i] = validFiveInput.nextLine();
+		}
+		return validwords5temp;
 	}
 
 	public static String[] importWords7() throws IOException {
@@ -62,7 +76,16 @@ public class Main extends JFrame {
 	 */
 	public static String[] getFive()
 	{
-		return WORDS5;
+		return words5;
+	}
+	
+	/**
+	 * getter method for "easy" (5 letter) words that are valid to guess
+	 * @return array of five letter valid words
+	 */
+	public static String[] getValidFive() 
+	{
+		return validwords5;
 	}
 
 	/**
@@ -71,6 +94,6 @@ public class Main extends JFrame {
 	 */
 	public static String[] getSeven()
 	{
-		return WORDS7;
+		return words7;
 	}
 }
