64deeb975d590ef6e4839cb5518ea42133529d5c
Shiza Charania <86497160+shizacharania@users.noreply.github.com>
2022-06-15 17:00:11 -0400

    add more APA citations

--- src/Credits.java
+++ src/Credits.java
@@ -1,7 +1,7 @@
 /**
- * Credits.java - version 1
+ * Credits.java - version 2
  * This class displays the credits/citations we used in APA format
- * @author shizacharania
+ * @author Shiza and Shirdel
  */
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -52,48 +52,56 @@ public class Credits extends JFrame implements ActionListener {
 		add(credit3b);
 		
 		// citation 4
-		JLabel credit4a = new JLabel("Five letter words: 660+ common 5 letter words in English • 7esl. 7ESL. (2021, June 13).");
+		JLabel credit4a = new JLabel("dracos. (n.d.). Valid-wordle-words.txt. Gist. Retrieved June 15, 2022, from");
 		credit4a.setBounds(5, -150, 800, 800);
 		add(credit4a);
-		JLabel credit4b = new JLabel("Retrieved June 8, 2022, from https://7esl.com/5-letter-words/");
+		JLabel credit4b = new JLabel("https://gist.github.com/dracos/dd0668f281e685bad51479e5acaadb93 ");
 		credit4b.setBounds(20, -130, 800, 800);
 		add(credit4b);
 		
 		// citation 5
 		JLabel credit5a = new JLabel("Five letter words: 660+ common 5 letter words in English • 7esl. 7ESL. (2021, June 13).");
-		credit5a.setBounds(5, -150, 800, 800);
+		credit5a.setBounds(5, -100, 800, 800);
 		add(credit5a);
 		JLabel credit5b = new JLabel("Retrieved June 8, 2022, from https://7esl.com/5-letter-words/");
-		credit5b.setBounds(20, -130, 800, 800);
+		credit5b.setBounds(20, -80, 800, 800);
 		add(credit5b);
 		
 		// citation 6
 		JLabel credit6a = new JLabel("Flowchart symbols and notation. Lucidchart. (n.d.).");
-		credit6a.setBounds(5, -100, 800, 800);
+		credit6a.setBounds(5, -50, 800, 800);
 		add(credit6a);
 		JLabel credit6b = new JLabel("Retrieved June 8, 2022, from https://www.lucidchart.com/pages/flowchart-symbols-meaning-explained");
-		credit6b.setBounds(20, -80, 800, 800);
+		credit6b.setBounds(20, -30, 800, 800);
 		add(credit6b);
 		
 		// citation 7
 		JLabel credit7a = new JLabel("Java programming tutorial. GUI Programming - Java Programming Tutorial. (n.d.).");
-		credit7a.setBounds(5, -50, 800, 800);
+		credit7a.setBounds(5, 0, 800, 800);
 		add(credit7a);
 		JLabel credit7b = new JLabel("Retrieved June 8, 2022, from https://www3.ntu.edu.sg/home/ehchua/programming/java/j4a_gui.html");
-		credit7b.setBounds(20, -30, 800, 800);
+		credit7b.setBounds(20, 20, 800, 800);
 		add(credit7b);
 		
 		// citation 8
 		JLabel credit8a = new JLabel("Java™ Platform, Standard Edition 8 API Specification. Java Platform SE 8. (n.d.).");
-		credit8a.setBounds(5, 0, 800, 800);
+		credit8a.setBounds(5, 50, 800, 800);
 		add(credit8a);
 		JLabel credit8b = new JLabel("Retrieved June 8, 2022, from https://docs.oracle.com/javase/8/docs/api/");
-		credit8b.setBounds(20, 20, 800, 800);
+		credit8b.setBounds(20, 70, 800, 800);
 		add(credit8b);
+		
+		// citation 8
+		JLabel credit9a = new JLabel("Where developers learn, share, &amp; build careers. Stack Overflow. (n.d.).");
+		credit9a.setBounds(5, 100, 800, 800);
+		add(credit9a);
+		JLabel credit9b = new JLabel("Retrieved June 15, 2022, from https://stackoverflow.com/");
+		credit9b.setBounds(20, 120, 800, 800);
+		add(credit9b);
 
 		// main menu button
 		back = new JButton("Back");
-		back.setBounds(225, 500, 150, 50);
+		back.setBounds(225, 550, 150, 50);
 		add(back);
 		back.addActionListener(this);
 		
@@ -118,7 +126,6 @@ public class Credits extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource() == back) {
-//			System.out.println("back");
 			// if back is pressed, we go back to the settings by calling it
 			new Settings();
 			dispose();
