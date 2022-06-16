fbe1b37957b5e8b47a28bbc86fd0e348cc5d7bb1
ShirdelY <shirdel.yan@gmail.com>
2022-06-15 22:45:07 -0400

    edit batch file

--- src/run.bat
+++ src/run.bat
@@ -1,12 +1,3 @@
-javac AddAccount.java
-javac Congratulations.java
-javac Credits.java
-javac Instructions.java
-javac Login.java
-javac Loser.java
 javac Main.java
-javac Mainmenu.java
-javac Settings.java
-javac Start.java
 pause
 java Main
