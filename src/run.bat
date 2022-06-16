419316a59f5790f7fced2f72a5be615e4e39c30c
Shiza Charania <86497160+shizacharania@users.noreply.github.com>
2022-06-15 22:48:15 -0400

    Update run.bat

--- src/run.bat
+++ src/run.bat
@@ -1,3 +1,13 @@
 javac Main.java
+javac AddAccount.java
+javac Congratulations.java
+javac Credits.java
+javac Game.java
+javac Instructions.java
+javac Login.java
+javac Loser.java
+javac Mainmenu.java
+javac Settings.java
+javac Start.java
 pause
 java Main
